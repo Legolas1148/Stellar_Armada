d937da9fd678f673b96befcf045b8d250fd4370e
Legolas1148 <112130283+Legolas1148@users.noreply.github.com>
2025-12-06 03:02:50 -0500

    Guard left clicks when not player's turn

--- Stellar_Armada/Source/Stellar_Armada/SA_PlayerController.h
+++ Stellar_Armada/Source/Stellar_Armada/SA_PlayerController.h
@@ -4,20 +4,27 @@
 #include "GameFramework/PlayerController.h"
 #include "SA_PlayerController.generated.h"
 
+class AActor;
+
 UCLASS()
 class ASA_PlayerController : public APlayerController
 {
-	GENERATED_BODY()
+        GENERATED_BODY()
 
 public:
-	virtual void SetupInputComponent() override;
+        virtual void SetupInputComponent() override;
 
 protected:
-	UPROPERTY(BlueprintReadOnly, Category = "Stellar Armada|Selection")
-	TObjectPtr<AActor> SelectedActor;
+        UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Stellar Armada|Turn", meta = (AllowPrivateAccess = "true"))
+        TObjectPtr<AActor> TurnManager;
 
-	UFUNCTION()
-	void HandleLeftClick();
+        UPROPERTY(BlueprintReadOnly, Category = "Stellar Armada|Selection")
+        TObjectPtr<AActor> SelectedActor;
+
+        bool IsPlayerTurnActive() const;
+
+        UFUNCTION()
+        void HandleLeftClick();
 
 	// Lets your Blueprint add visuals like highlight rings
 	UFUNCTION(BlueprintImplementableEvent, Category = "Stellar Armada|Selection")
